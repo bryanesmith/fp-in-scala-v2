c2a737537090264d75579d6a006366b26cec044d
Bryan Smith <bryanesmith@gmail.com>
2017-12-10 14:55:07 -0500

    exercise 3.29

--- src/main/scala/ch3/Tree.scala
+++ src/main/scala/ch3/Tree.scala
@@ -8,13 +8,7 @@ object Tree {
 
   implicit class IntTreeOps (t: Tree[Int]) {
     // Exercise 3.26
-    def maximum: Int = {
-      def go(t1: Tree[Int]): Int = t1 match {
-        case Branch(l, r) => go(l).max(go(r))
-        case Leaf(a) => a
-      }
-      go(t)
-    }
+    def maximum: Int = t.fold(Integer.MIN_VALUE) {(a,b) => a max b }
   }
 
   /**
@@ -24,14 +18,24 @@ object Tree {
 
     def :+(other:Tree[A]): Tree[A] = Branch(t, other)
 
-    // Exercise 3.25
-    def size: Int = {
-      def go(t1: Tree[A]): Int = t1 match {
-        case Branch(l, r) => 1 + go(l) + go(r)
-        case _ => 1
+    // Exercise 3.29
+    def fold[B](z: B)(f: (A, B) => B): B =
+      t.traverse(z)((t1, b) => t1 match {
+        case Leaf(a) => f(a, b)
+        case _ => b
+      })
+
+    // Exercise 3.29
+    def traverse[B](z: B)(f: (Tree[A], B) => B): B = {
+      def go(t1: Tree[A], acc: B): B = t1 match {
+        case Leaf(_) => f(t1, acc)
+        case Branch(l, r) => go(r, go(l, f(t1, acc)))
       }
-      go(t)
+      go(t, z)
     }
+
+    // Exercise 3.25
+    def size: Int = t.traverse(0) {(_, b) => b + 1}
 
     // Exercise 3.27
     def depth: Int = {
