1bb031d0bc419d7a3318f2d6720ec5ce0548544b
Bryan Smith <bryanesmith@gmail.com>
2017-12-09 20:32:12 -0500

    introduct ListOps so functions are infix

--- src/main/scala/ch3/List.scala
+++ src/main/scala/ch3/List.scala
@@ -21,41 +21,56 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  // To avoid prefix-based function...
+  implicit class ListOps[A] (l: List[A]) {
+
+    def foldRight[B](z: B)(f: (A, B) => B): B = {
+      def go(as: List[A], z: B): B = as match {
+        case Nil => z
+        case Cons(x, xs) => f(x, go(xs, z))
+      }
+      go(l, z)
     }
 
-  // Exercise 3.2
-  def tail[A](as:List[A]): List[A] = drop(as, 1)
+    // Exercise 3.2
+    def tail: List[A] = drop(1)
 
-  // Exercise 3.3
-  def setHead[A](a:A, as:List[A]) = Cons(a, as)
+    // Exercise 3.3
+    def setHead(a:A) = Cons(a, l)
 
-  // Exercise 3.4
-  @annotation.tailrec
-  def drop[A](as: List[A], n: Int): List[A] = as match {
-    case Nil => Nil
-    case Cons(_:A, tail:List[A]) if n > 0 => drop(tail, n - 1)
-    case _ => as
+    // Exercise 3.4
+    def drop(num: Int): List[A] = {
+      @annotation.tailrec
+      def go(as: List[A], n: Int): List[A] = as match {
+        case Nil => Nil
+        case Cons(_:A, tail:List[A]) if n > 0 => go(tail, n - 1)
+        case _ => as
+      }
+      go(l, num)
+    }
+
+    // Exercise 3.5
+    def dropWhile(f: A => Boolean): List[A] = {
+      @annotation.tailrec
+      def go(as: List[A]): List[A] = as match {
+        case Nil => Nil
+        case Cons(head:A, tail:List[A]) if f(head) => go(tail)
+        case _ => as
+      }
+      go(l)
+    }
+
+    // Exercise 3.6
+    def init: List[A] = {
+      def go(as: List[A]): List[A] = as match {
+        case Nil => Nil
+        case Cons(a:A, Nil) => Nil
+        case Cons(head:A, tail:List[A]) => Cons(head, go(tail))
+      }
+      go(l)
+    }
+
   }
-
-  // Exercise 3.5
-  @annotation.tailrec
-  def dropWhile[A](as: List[A], f: A => Boolean): List[A] = as match {
-    case Nil => Nil
-    case Cons(head:A, tail:List[A]) if f(head) => dropWhile(tail, f)
-    case _ => as
-  }
-
-  // Exercise 3.6
-  def init[A](l: List[A]): List[A] = l match {
-    case Nil => Nil
-    case Cons(a:A, Nil) => Nil
-    case Cons(head:A, tail:List[A]) => Cons(head, init(tail))
-  }
-
-
 
 }
+
--- src/test/scala/ch3/ListTest.scala
+++ src/test/scala/ch3/ListTest.scala
@@ -5,21 +5,17 @@ import org.scalatest.FlatSpec
 
 class ListTest extends FlatSpec {
 
-  "List.sum" should "handle empty list" in {
+  "List.sum" should "handle empty list" in
     assert(sum(Nil) == 0)
-  }
 
-  it should "handle non-empty list" in {
+  it should "handle non-empty list" in
     assert(sum(List(1,2,3)) == 6)
-  }
 
-  "List.product" should "handle empty list" in {
+  "List.product" should "handle empty list" in
     assert(product(Nil) == 1)
-  }
 
-  it should "handle non-empty list" in {
+  it should "handle non-empty list" in
     assert(product(List(2.0, 2.0, 4.0)) == 16.0)
-  }
 
   // exercise 3.1
   "List" should "solve exercise 3.1" in {
@@ -33,79 +29,67 @@ class ListTest extends FlatSpec {
     assert(found == 3)
   }
 
-  "List.tail" should "handle an empty list" in {
-    assert(tail(Nil) == Nil)
-  }
+  "List.tail" should "handle an empty list" in
+    assert(Nil.tail == Nil)
 
-  it should "handle a list of one" in {
-    assert(tail(List(1)) == Nil)
-  }
+  it should "handle a list of one" in
+    assert(List(1).tail == Nil)
 
-  it should "handle a non-empty list" in {
-    assert(tail(List(1, 2, 3)) == List(2, 3))
-  }
+  it should "handle a non-empty list" in
+    assert(List(1, 2, 3).tail == List(2, 3))
 
-  "List.setHead" should "handle an empty list" in {
-    assert(setHead(1, Nil) == List(1))
-  }
+  "List.setHead" should "handle an empty list" in
+    assert(Nil.setHead(1) == List(1))
 
-  it should "handle an non-empty list" in {
-    assert(setHead(1, List(2, 3)) == List(1, 2, 3))
-  }
+  it should "handle an non-empty list" in
+    assert(List(2, 3).setHead(1) == List(1, 2, 3))
 
-  "List.drop" should "handle an empty list" in {
-    assert(drop(Nil, 1) == Nil)
-  }
+  "List.drop" should "handle an empty list" in
+    assert(Nil.drop(1) == Nil)
 
-  it should "handle negative n" in {
-    assert(drop(List(1, 2, 3), -11) == List(1, 2, 3))
-  }
+  it should "handle negative n" in
+    assert(List(1, 2, 3).drop(-11) == List(1, 2, 3))
 
   it should "handle n less than length of list" in {
     val l = List(1, 2, 3)
-    assert(drop(l, 1) == List(2, 3))
-    assert(drop(l, 2) == List(3))
+    assert(l.drop(1) == List(2, 3))
+    assert(l.drop(2) == List(3))
   }
 
-  it should "handle n equal to length of list" in {
-    assert(drop(List(1, 2, 3), 3) == Nil)
-  }
+  it should "handle n equal to length of list" in
+    assert(List(1, 2, 3).drop(3) == Nil)
 
-  it should "handle n greater length of list" in {
-    assert(drop(List(1, 2, 3), 4) == Nil)
-  }
+  it should "handle n greater length of list" in
+    assert(List(1, 2, 3).drop(4) == Nil)
 
-  "List.dropWhile" should "handle an empty list" in {
-    assert(dropWhile(Nil, (_: Int) => false) == Nil)
-  }
+  "List.dropWhile" should "handle an empty list" in
+    assert(Nil.dropWhile { _: Int => false } == Nil)
 
-  it should "handle a predicate that fails on first ele" in {
-    assert(dropWhile(List(1, 2, 3), (_: Int) => false) == List(1, 2, 3))
-  }
+  it should "handle a predicate that fails on first ele" in
+    assert(List(1, 2, 3).dropWhile { _ => false } == List(1, 2, 3))
 
   it should "handle a predicate that succeeds on some ele" in {
     val l = List(1, 2, 3)
-    assert(dropWhile(l, (a: Int) => a <= 1) == List(2, 3))
-    assert(dropWhile(l, (a: Int) => a <= 2) == List(3))
+    assert(l.dropWhile { _ <= 1 } == List(2, 3))
+    assert(l.dropWhile { _ <= 2 } == List(3))
   }
 
-  it should "handle a predicate that succeeds on all ele" in {
-    assert(dropWhile(List(1, 2, 3), (_: Int) => true) == Nil)
-  }
+  it should "handle a predicate that succeeds on all ele" in
+    assert(List(1, 2, 3).dropWhile { _ => true } == Nil)
 
   "List.init" should "handle an empty list" in {
-    assert(init(Nil) == Nil)
+    assert(Nil.init == Nil)
+    assert(Nil.init.init == Nil)
   }
 
   it should "handle a non-empty list" in {
-    assert(init(List(1)) == Nil)
-    assert(init(List(1, 2)) == List(1))
-    assert(init(List(1, 2, 3)) == List(1, 2))
+    assert(List(1).init == Nil)
+    assert(List(1, 2).init == List(1))
+    assert(List(1, 2, 3).init == List(1, 2))
   }
 
   // Exercise 3.8
-  "List.tail" should "solve exercise 3.8" in {
-    val found = foldRight(List(1,2,3), Nil: List[Int])(Cons(_,_))
-    assert(found == List(1, 2, 3))
-  }
+  "List.tail" should "solve exercise 3.8" in
+    assert( List(1, 2, 3) == List(1,2,3).foldRight(Nil: List[Int]){ Cons(_,_) } )
+
 }
