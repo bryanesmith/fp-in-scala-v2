69a6b77cf1ae1c013a31ef1f15ba7d7260beedd5
Bryan Smith <bryanesmith@gmail.com>
2017-12-22 11:26:43 -0500

    add worst-case runtimes

--- src/main/scala/ch5/Stream.scala
+++ src/main/scala/ch5/Stream.scala
@@ -7,27 +7,28 @@ sealed trait Stream[+A] {
     case Cons(h, _) => Some(h())
   }
 
-  // Exercise 5.1
+  // Exercise 5.1 - O(n^2)
   def toList: List[A] = {
     @annotation.tailrec
     def go(s: Stream[A], acc: List[A]): List[A] = s match {
       case Empty => acc
-      case Cons(h, t) => go(t(), acc :+ h())
+      case Cons(h, t) => go(t(), acc :+ h()) // Warning: append is O(n)
     }
 
     go(this, Nil)
   }
 
+  // Exercise 5.2 - O(n^2)
   def take(n: Int): Stream[A] = {
     @annotation.tailrec
     def go(s: Stream[A], r: Int, acc: List[A]): Stream[A] = s match {
-      case Cons(h, t) if r > 0 => go(t(), r - 1, acc :+ h())
+      case Cons(h, t) if r > 0 => go(t(), r - 1, acc :+ h()) // Warning: append is O(n)
       case _ => Stream(acc: _*)
     }
     go(this, n, Nil)
   }
 
-  // Exercise 5.2
+  // Exercise 5.2 - O(n)
   @annotation.tailrec
   final def drop(n: Int): Stream[A] = this match {
     case Cons(_, t) if n > 0 => t().drop(n - 1)
