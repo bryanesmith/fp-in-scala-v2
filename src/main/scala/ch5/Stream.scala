943687590e3754552e1d02103513234a40b1c92c
Bryan Smith <bryanesmith@gmail.com>
2017-12-22 11:10:00 -0500

    exercise 5.2

--- src/main/scala/ch5/Stream.scala
+++ src/main/scala/ch5/Stream.scala
@@ -4,7 +4,7 @@ sealed trait Stream[+A] {
 
   def headOption: Option[A] = this match {
     case Empty => None
-    case Cons(h, t) => Some(h())
+    case Cons(h, _) => Some(h())
   }
 
   // Exercise 5.1
@@ -13,6 +13,19 @@ sealed trait Stream[+A] {
     case Cons(h, t) => h() :: t().toList
   }
 
+  // Exercise 5.2
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => Cons(h, () => t().take(n-1))
+    case _ => Empty // Empty or n <= 0
+  }
+
+  // Exercise 5.2
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ if n == 0 => this
+    case _ => Empty  // Empty or n < 0
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
--- src/test/scala/ch5/StreamTest.scala
+++ src/test/scala/ch5/StreamTest.scala
@@ -11,4 +11,40 @@ class StreamTest extends FlatSpec {
     assert(Stream("a", "b").toList == List("a", "b"))
     assert(Stream("a", "b", "c").toList == List("a", "b", "c"))
   }
+
+  "take" should "handle negative n" in
+    assert(Stream(1, 2, 3).take(-1) == Empty)
+
+  it should "handle Empty" in {
+    assert(Stream.empty.take(0) == Empty)
+    assert(Stream.empty.take(1) == Empty)
+  }
+
+  it should "handle cases where n less than or equal to stream length" in {
+    assert(Stream(1, 2, 3).take(0) == Stream.empty)
+    assert(Stream(1, 2, 3).take(1).toList == List(1))
+    assert(Stream(1, 2, 3).take(2).toList == List(1, 2))
+    assert(Stream(1, 2, 3).take(3).toList == List(1, 2, 3))
+  }
+
+  it should "handle cases where n greater than or equal to stream length" in
+    assert(Stream(1, 2, 3).take(4).toList == List(1, 2, 3))
+
+  "drop" should "handle negative n" in
+    assert(Stream(1, 2, 3).drop(-1) == Empty)
+
+  it should "handle Empty" in {
+    assert(Stream.empty.drop(0) == Empty)
+    assert(Stream.empty.drop(1) == Empty)
+  }
+
+  it should "handle cases where n less than or equal to stream length" in {
+    assert(Stream(1, 2, 3).drop(0).toList == List(1, 2, 3))
+    assert(Stream(1, 2, 3).drop(1).toList == List(2, 3))
+    assert(Stream(1, 2, 3).drop(2).toList == List(3))
+    assert(Stream(1, 2, 3).drop(3) == Empty)
+  }
+
+  it should "handle cases where n greater than or equal to stream length" in
+    assert(Stream(1, 2, 3).drop(4) == Empty)
 }
