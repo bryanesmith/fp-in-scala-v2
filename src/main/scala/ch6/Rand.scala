ee1094cddc187d23ded7c6cbb0e0c791626c198d
Bryan Smith <bryanesmith@gmail.com>
2018-01-06 12:47:39 -0500

    exercise 6.6

--- src/main/scala/ch6/Rand.scala
+++ src/main/scala/ch6/Rand.scala
@@ -11,4 +11,12 @@ object Rand {
       val (a, rng2) = s(rng)
       (f(a), rng2)
     }
+
+  // Exercise 6.6
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a,b), rng3)
+    }
 }
